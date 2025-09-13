e61915684c54351a6aa907bba3d32950a0e9bf06
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2025-09-13 22:55:21 +0000

    update toolchain

--- ci/jobs/fuzz.sh
+++ ci/jobs/fuzz.sh
@@ -8,7 +8,7 @@ cd ../..
 LIBFUZZ_RUN_ARGS="--verbose --jobs=$(nproc) -- -runs=10000000 -timeout=5 -print_final_stats=1 -print_pcs=1"
 
 cd fuzz
-echo ">> hfuzz fuzz-behavior-matches-lru"
+echo ">> fuzz-behavior-matches-lru"
 cargo +nightly-2025-06-27 fuzz run fuzz-behavior-matches-lru $LIBFUZZ_RUN_ARGS
 
 echo ">> fuzz-internal-invariants"
